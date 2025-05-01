dbe25dc02b19a185da3227bcf9ad50864b0805b4
Saidamin777 <saidakmalov1707@gmail.com>
2025-04-30 21:33:46 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+let ism = prompt('ismingizni kiriting')
+
+let idd = +prompt('space idingizni kiriting')
+
+let login = +prompt('space passwordingizni kiriting')
+
+const obj = {
+    names: ism,
+    ID: idd,
+    password: login,
+}
+console.log(obj);
+ alert('space accountingizga xush kelibsiz')
